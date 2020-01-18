6acd6f01986b4d16d8a29372a8a49ae856befa68
Matsiukhov Dzmitry <dimmarvel@mail.ru>
2020-01-18 10:26:54 +0300

    Func for calc contr bit

--- Hamming_Code/Hammind_Code.cpp
+++ Hamming_Code/Hammind_Code.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
 #include<cstring>
 #include<map>
-void cout_arr(int *arr, int size) {
+enum PLUS_SIZE;
+PLUS_SIZE get_next_ps(PLUS_SIZE p_s);
+void cout_arr(int *bit_array, int size) {
 	for (int i = 1; i < size + 1; i++)
 	{
-		std::cout << " " << arr[i];
+		std::cout << " " << bit_array[i];
 	}
 	std::cout << std::endl;
 }
-void init_arr(int *arr, int size) {
+void init_arr(int *bit_array, int size) {
 	for (int i = 1; i < size + 1; i++) {
-		arr[i] = i;
+		bit_array[i] = i;
 	}
 }
 enum PLUS_SIZE
@@ -19,6 +21,51 @@ enum PLUS_SIZE
 	FOUR = 8, FIVE = 16, SIX = 32,
 	SEVEN = 64, EIGHT = 128, NINE = 256,
 };
+int* create_bit_array(int **arr_bit, int size_h, int size_a, int size_numbers, int *arr_numbers, PLUS_SIZE p_s) {
+	PLUS_SIZE fnc_p_c = { ONE };
+	int size_char_bit_arr = size_h + size_a;
+	int *char_bit_arr = new int[size_char_bit_arr];
+	int *bit_and_contrbit_arr = new int[size_char_bit_arr];
+	std::cout << "ha:\n" << "h - ";
+	for (int i = 0; i < size_h; i++)
+	{
+		char_bit_arr[i] = arr_bit[0][i];
+		std::cout << char_bit_arr[i];
+	}
+	std::cout << std::endl;
+	std::cout << "a - ";
+	for (int i = 0; i < size_a; i++)
+	{
+		char_bit_arr[size_h + i] = arr_bit[1][i];
+		std::cout << char_bit_arr[size_h + i];
+	}
+	std::cout << std::endl;
+	std::cout << "ha - ";
+	for (int i = 0; i < size_char_bit_arr; i++)
+	{
+		std::cout << char_bit_arr[i];
+	}
+	int j = 0;
+	std::cout << std::endl;
+	for (int i = 1; i <= size_numbers; i++)
+	{
+		if (i == fnc_p_c) {
+			bit_and_contrbit_arr[i] = 0;
+			fnc_p_c = get_next_ps(fnc_p_c);
+		}
+		else {
+			bit_and_contrbit_arr[i] = char_bit_arr[j++];
+			if(i >= fnc_p_c)
+				fnc_p_c = get_next_ps(fnc_p_c);
+		}
+	}
+	std::cout << "\nbit_and_contrbit_arr - ";
+	for (int i = 1; i <= size_numbers; i++)
+	{
+		std::cout << bit_and_contrbit_arr[i];
+	}
+	return bit_and_contrbit_arr;
+}
 PLUS_SIZE get_next_ps(PLUS_SIZE p_s) {
 	return static_cast<PLUS_SIZE>(static_cast<int>(p_s) * 2);
 }
@@ -29,23 +76,37 @@ PLUS_SIZE calc_size(PLUS_SIZE p_s, int index, int i = 0) {
 	}
 	return p_s;
 }
+bool X_counter(int* arr_bit,PLUS_SIZE p_s,int size_bit_arr) {
+	//true - чет
+	//false - не чет
+
+}
 int main() {
 	setlocale(LC_ALL, "rus");
-	char h[8] = { '0','1','0' ,'0' ,'0' ,'1','0', '0' };
-	char a[8] = { '0','0','1' ,'1' ,'1' ,'1','0', '1' };
-	int two_pow_arr[9] =
+	int h[8] = { 0,1,0,0,0,1,0,0 };
+	int a[8] = { 0,0,1,1,1,1,0,1 };
+	int size_bit_sum = (sizeof(h) / sizeof(int)) + (sizeof(a) / sizeof(int));
+	int size_bit_h = sizeof(h) / sizeof(int);
+	int size_bit_a = sizeof(a) / sizeof(int);
+	int *word[2];
+	word[0] = new int[size_bit_h];
+	word[1] = new int[size_bit_a];
+	word[0] = h;
+	word[1] = a;
+
+	int two_pow_bit_array[9] =
 	{
 		1, 2, 4,
 		8, 16, 32,
 		64, 128, 256
 	};
-	PLUS_SIZE plus_size = {ONE};
-	int bit_sum = sizeof(h) + sizeof(a);
-	std::cout << "Всего бит: " << bit_sum << std::endl;
-	int index = bit_sum;
-	int *arr = new int[index];
-	init_arr(arr, index);
-	cout_arr(arr, index);
+	PLUS_SIZE plus_size = { ONE };
+
+	std::cout << "Всего бит: " << size_bit_sum << std::endl;
+	int index = size_bit_sum;
+	int *bit_arr = new int[index];
+	init_arr(bit_arr, index);
+	cout_arr(bit_arr, index);
 
 	plus_size = calc_size(plus_size, index);
 	switch (plus_size) {
@@ -95,9 +156,19 @@ int main() {
 		std::cout << index << std::endl;
 		break;
 	}
-	int *arr2 = new int[index];
-	init_arr(arr2, index);
-	cout_arr(arr2, index);
+
+	int *BitContr_arr = new int[index];
+	init_arr(BitContr_arr, index);
+	cout_arr(BitContr_arr, index);
+
+	int *bit_and_contr_bit = new int[index];
+	bit_and_contr_bit = create_bit_array(word, size_bit_h, size_bit_a, index, BitContr_arr, plus_size);
+
+	bool chetnost = X_counter(bit_and_contr_bit, plus_size,index);
+
+
+
+
 	system("pause");
 	return 0;
 }
